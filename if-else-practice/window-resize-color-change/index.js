21abc8625119198439488e700093b84f0cbac3c0
Danim Kim <74545780+dahhnym@users.noreply.github.com>
2021-09-25 21:13:06 +0900

    Create index.js

---
+++ if-else-practice/window-resize-color-change/index.js
@@ -0,0 +1,14 @@
+let width = window.innerWidth;
+
+function handleWindowResize() {
+  width = window.innerWidth;
+  //console.log("브라우저너비: " + width);
+  if (width < 600) {
+    document.body.style.backgroundColor = "#1abc9c";
+  } else if (width < 900) {
+    document.body.style.backgroundColor = "#3498db";
+  } else {
+    document.body.style.backgroundColor = "#f39c12";
+  }
+}
+window.addEventListener("resize", handleWindowResize);
